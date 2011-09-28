ff9ca9eb2fa6a1f3e53f12708bc7deb06e7b3b7b
Miłosz Kosobucki <milosz@kosobucki.pl>
2011-09-28 17:07:55 +0200

    Initial revision

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include<gtk/gtk.h>
+#include<glib/gi18n.h>
+
+int main(int argc, char **argv) {
+	GtkWidget* window;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Obrazator");
+
+	g_signal_connect(window, "destroy", 
+		G_CALLBACK (gtk_main_quit), NULL);
+	
+	gtk_init(&argc, &argv);
+	gtk_widget_show_all(window);
+
+	gtk_main();
+	return 0;
+}
