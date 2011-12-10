130da59470d3020b1ad1faf2568c5f0eecca834e
Michał Siejak <michal@siejak.pl>
2011-12-10 13:12:34 +0100

    Beginning implementation of CL glue code.

---
+++ include/device.h
@@ -0,0 +1,15 @@
+#ifndef __CLIT_DEVICE_H
+#define __CLIT_DEVICE_H
+
+enum device_result_e {
+    DEVICE_OK = 0x00,
+    DEVICE_EUNAVAIL,
+    DEVICE_ERROR,
+};
+typedef device_result_e device_result_t;
+
+device_result_t device_init(void);
+    
+    
+
+#endif
